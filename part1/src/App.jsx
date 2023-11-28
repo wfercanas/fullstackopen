2c3386172283f6e737d7f255c6e0162bd466bb03
wfercanas <fernando.canas@outlook.com>
2023-11-27 21:08:00 -0500

    feat(part1): exercise 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,91 +1,40 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
-
-function StatisticLine(props) {
-  const { text, value, unit } = props;
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>
-        {value} {unit}
-      </td>
-    </tr>
-  );
-}
-StatisticLine.propTypes = {
-  text: PropTypes.string,
-  value: PropTypes.number,
-  unit: PropTypes.string,
-};
-
-function Statistics(props) {
-  const { good, neutral, bad, total } = props;
-  if (total === 0) {
-    return <p>No feedback given</p>;
-  }
-
-  return (
-    <>
-      <h2>Statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={total} />
-          <StatisticLine text="average" value={(good - bad) / total} />
-          <StatisticLine
-            text="positive feedback"
-            value={(good / total) * 100}
-            unit="%"
-          />
-        </tbody>
-      </table>
-    </>
-  );
-}
-Statistics.propTypes = {
-  good: PropTypes.number,
-  neutral: PropTypes.number,
-  bad: PropTypes.number,
-  total: PropTypes.number,
-};
-
-function Button(props) {
-  const { onClick, label } = props;
-  return <button onClick={onClick}>{label}</button>;
-}
-Button.propTypes = {
-  onClick: PropTypes.func,
-  label: PropTypes.string,
-};
+import Unicafe from "./Unicafe";
+import Anecdotes from "./Anecdotes";
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [display, setDisplay] = useState("unicafe");
 
-  let total = good + neutral + bad;
-
-  function handleGood() {
-    setGood(good + 1);
-  }
-
-  function handleNeutral() {
-    setNeutral(neutral + 1);
-  }
-
-  function handleBad() {
-    setBad(bad + 1);
+  function handleChange(event) {
+    setDisplay(event.target.value);
   }
 
   return (
     <>
-      <h1>Give Feedback</h1>
-      <Button onClick={handleGood} label="good" />
-      <Button onClick={handleNeutral} label="neutral" />
-      <Button onClick={handleBad} label="bad" />
-      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
+      <form>
+        <label>
+          <input
+            name="display"
+            value="unicafe"
+            type="radio"
+            onChange={handleChange}
+            checked={display === "unicafe"}
+          />{" "}
+          Unicafe
+        </label>
+        <label>
+          <input
+            name="display"
+            value="anecdotes"
+            type="radio"
+            onChange={handleChange}
+            checked={display === "anecdotes"}
+          />{" "}
+          Anecdotes
+        </label>
+      </form>
+
+      {display === "unicafe" ? <Unicafe /> : <Anecdotes />}
     </>
   );
 }
---
+++ part1/src/Unicafe.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+function StatisticLine(props) {
+  const { text, value, unit } = props;
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>
+        {value} {unit}
+      </td>
+    </tr>
+  );
+}
+StatisticLine.propTypes = {
+  text: PropTypes.string,
+  value: PropTypes.number,
+  unit: PropTypes.string,
+};
+
+function Statistics(props) {
+  const { good, neutral, bad, total } = props;
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={(good - bad) / total} />
+          <StatisticLine
+            text="positive feedback"
+            value={(good / total) * 100}
+            unit="%"
+          />
+        </tbody>
+      </table>
+    </>
+  );
+}
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+};
+
+function Button(props) {
+  const { onClick, label } = props;
+  return <button onClick={onClick}>{label}</button>;
+}
+Button.propTypes = {
+  onClick: PropTypes.func,
+  label: PropTypes.string,
+};
+
+function Unicafe() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  let total = good + neutral + bad;
+
+  function handleGood() {
+    setGood(good + 1);
+  }
+
+  function handleNeutral() {
+    setNeutral(neutral + 1);
+  }
+
+  function handleBad() {
+    setBad(bad + 1);
+  }
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGood} label="good" />
+      <Button onClick={handleNeutral} label="neutral" />
+      <Button onClick={handleBad} label="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
+    </>
+  );
+}
+
+export default Unicafe;
